60d7f73d94082a1de4abf86c1403c745c12a88b9
bathiyasid <bathiya.siddharth@gmail.com>
2021-05-10 20:26:56 +0530

    Create RfiD Simulation.ino

---
+++ RfiD Simulation/RfiD Simulation.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Arduino RFID Card Simulation Activated");
+  delay(1000);
+}
